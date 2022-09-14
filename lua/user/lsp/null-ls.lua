6a3fb9cac16c1e2786c0676ac5de39ca9fe06a55
Freddy Hauteville <freddy.hauteville@groupeseloger.com>
2022-09-14 17:07:37 +0200

    feat(null-ls): configure python

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -18,7 +18,7 @@ null_ls.setup {
     },
     formatting.black.with { extra_args = { "--fast" } },
     formatting.stylua,
-    formatting.google_java_format,
-    diagnostics.flake8,
+    formatting.isort,
+    diagnostics.flake8.with { extra_args = { "--config ~/.config/flake8" } },
   },
 }
